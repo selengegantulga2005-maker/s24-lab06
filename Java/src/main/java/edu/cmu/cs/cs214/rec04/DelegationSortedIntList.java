00953864b5d1ffe81adec01b1bb6ff63f42866e1
Selenge <selenge.gantulga.2005@gmail.com>
2026-03-10 16:29:20 +0800

    Lab 6 ажилласан, Бүх тестийг ажиллуулав

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -1,37 +1,43 @@
 package edu.cmu.cs.cs214.rec04;
 
-
-
 public class DelegationSortedIntList implements IntegerList {
-    private SortedIntList list = new SortedIntList(); // Хувийн объект үүсгэх [cite: 33]
-    private int totalAdded = 0; // [cite: 91]
+
+    private SortedIntList list = new SortedIntList();
+    private int totalAdded = 0;
 
     @Override
     public boolean add(int value) {
-        totalAdded++; // Нэмэх үйлдэл бүрийг тооцно [cite: 35]
-        return list.add(value); // Үйлдлийг дамжуулах [cite: 34]
+        totalAdded++;
+        return list.add(value);
     }
 
     @Override
     public boolean addAll(IntegerList items) {
-        totalAdded += items.size(); // [cite: 35]
-        return list.addAll(items); // [cite: 34]
+        totalAdded += items.size();
+        return list.addAll(items);
     }
 
     public int getTotalAdded() {
-        return totalAdded; // [cite: 92]
+        return totalAdded;
     }
 
-    // Бусад IntegerList-ийн функцуудыг мөн дамжуулах ёстой [cite: 34]
     @Override
-    public int get(int index) { return list.get(index); }
+    public int get(int index) {
+        return list.get(index);
+    }
 
     @Override
-    public boolean remove(int value) { return list.remove(value); }
+    public boolean remove(int value) {
+        return list.remove(value);
+    }
 
     @Override
-    public boolean removeAll(IntegerList items) { return list.removeAll(items); }
+    public boolean removeAll(IntegerList items) {
+        return list.removeAll(items);
+    }
 
     @Override
-    public int size() { return list.size(); }
+    public int size() {
+        return list.size();
+    }
 }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -1,17 +1,8 @@
 package edu.cmu.cs.cs214.rec04;
 
-/**
- * InheritanceSortedIntList -- a variant of a SortedIntList that keeps
- * count of the number of attempted element insertions (not to be confused
- * with the current size, which goes down when an element is removed)
- * and exports an accessor (totalAdded) for this count.
- *
- * @author Nora Shoemaker
- *
- */
-
 public class InheritanceSortedIntList extends SortedIntList {
-    private int totalAdded = 0; 
+
+    private int totalAdded = 0;
 
     @Override
     public boolean add(int value) {
@@ -21,11 +12,10 @@ public class InheritanceSortedIntList extends SortedIntList {
 
     @Override
     public boolean addAll(IntegerList list) {
-
         return super.addAll(list);
     }
 
     public int getTotalAdded() {
-        return totalAdded; 
+        return totalAdded;
     }
 }
