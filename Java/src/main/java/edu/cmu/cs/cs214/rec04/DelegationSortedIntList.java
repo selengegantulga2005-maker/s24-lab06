513e2fe68242e751f1d2c1e18f31b492585c4d65
Selenge <selenge.gantulga.2005@gmail.com>
2026-03-10 15:28:04 +0800

    Zassan ni

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -1,16 +1,37 @@
 package edu.cmu.cs.cs214.rec04;
 
-/**
- * DelegationSortedIntList -- a variant of a SortedIntList that keeps
- * count of the number of attempted element insertions (not to be confused
- * with the current size, which goes down when an element is removed)
- * and exports an accessor (totalAdded) for this count.
- *
- * @author Nora Shoemaker
- *
- */
 
-public class DelegationSortedIntList {
-    // Write your implementation below with API documentation
 
+public class DelegationSortedIntList implements IntegerList {
+    private SortedIntList list = new SortedIntList(); // Хувийн объект үүсгэх [cite: 33]
+    private int totalAdded = 0; // [cite: 91]
+
+    @Override
+    public boolean add(int value) {
+        totalAdded++; // Нэмэх үйлдэл бүрийг тооцно [cite: 35]
+        return list.add(value); // Үйлдлийг дамжуулах [cite: 34]
+    }
+
+    @Override
+    public boolean addAll(IntegerList items) {
+        totalAdded += items.size(); // [cite: 35]
+        return list.addAll(items); // [cite: 34]
+    }
+
+    public int getTotalAdded() {
+        return totalAdded; // [cite: 92]
+    }
+
+    // Бусад IntegerList-ийн функцуудыг мөн дамжуулах ёстой [cite: 34]
+    @Override
+    public int get(int index) { return list.get(index); }
+
+    @Override
+    public boolean remove(int value) { return list.remove(value); }
+
+    @Override
+    public boolean removeAll(IntegerList items) { return list.removeAll(items); }
+
+    @Override
+    public int size() { return list.size(); }
 }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,26 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+public class InheritanceSortedIntList extends SortedIntList {
+    private int totalAdded = 0; // Нийт нэмэгдсэн элементийн тоог хадгалах [cite: 82]
 
+    @Override
+    public boolean add(int value) {
+        // Элемент нэмэх оролдлого бүрд тоолуурыг нэмэгдүүлнэ [cite: 25]
+        totalAdded++;
+        return super.add(value);
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        // Жагсаалтаар элемент нэмэхэд түүний хэмжээгээр тоолуурыг нэмэгдүүлнэ [cite: 25, 26]
+        // Анхаар: AbstractIntList-ийн addAll нь add-г дотроо дууддаг бол 
+        // тоолуур давхардаж магадгүйг анхаараарай 
+        totalAdded += list.size();
+        return super.addAll(list);
+    }
+
+    public int getTotalAdded() {
+        return totalAdded; // Нийт нэмэгдсэн тоог буцаах [cite: 88]
+    }
 }
